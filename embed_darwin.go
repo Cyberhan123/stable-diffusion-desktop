dbd6218b708c299189b08c1390ffa7561584493c
Cyberhan123 <255542417@qq.com>
2024-01-21 12:11:11 +0800

    temporarily remove embedd

--- embed_darwin.go
+++ embed_darwin.go
@@ -1,4 +1,5 @@
 package main
 
-//go:embed backend/stable-diffusion-cpp/stable-diffusion-cpp-backend
-var StableDiffusionCppBackend []byte
+//
+////go:embed backend/stable-diffusion-cpp/stable-diffusion-cpp-backend
+//var StableDiffusionCppBackend []byte
